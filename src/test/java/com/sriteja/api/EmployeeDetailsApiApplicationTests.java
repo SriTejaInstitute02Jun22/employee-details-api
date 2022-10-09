d9612cad566a6cd585f0e9f24865bf999f1b6167
ramesh cheppali <ramesh96420@gmail.com>
2022-10-09 10:36:12 +0530

    initial commit

---
+++ src/test/java/com/sriteja/api/EmployeeDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriteja.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
